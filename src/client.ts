4c60982570c4504a16e00fac462b991d1b283c1b
dhythm <yuta.okada.20@gmail.com>
2022-10-30 12:13:01 +0900

    feat(app): implement client / server by Quickstart

---
+++ src/client.ts
@@ -0,0 +1,14 @@
+// @filename: client.ts
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import type { AppRouter } from "./server";
+const trpc = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3000/trpc",
+    }),
+  ],
+});
+
+// Inferred types
+const user = await trpc.userById.query("1");
+const createdUser = await trpc.userCreate.mutate({ name: "sachinraja" });
